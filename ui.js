5e0c125fc59d7c3c99323593a1a2919c6b75c8fa
Vishal Kumar <vishalkumarvk082@gmail.com>
2018-11-03 15:01:02 +0530

    added details and links with reset

---
+++ ui.js
@@ -0,0 +1,17 @@
+// var start = document.getElementById('startRotationbtn')
+// var stop = document.getElementById('stopRotationbtn')
+var reset = document.getElementById('resetBtn')
+
+// start.addEventListener('click', function(){
+
+// })
+
+// stop.addEventListener('click', function(){
+
+// })
+
+reset.addEventListener('click', function(){
+    location.reload()
+})
+
+
